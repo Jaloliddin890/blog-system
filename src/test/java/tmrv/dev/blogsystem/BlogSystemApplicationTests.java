65bc4a429d91290a8adf3ff524e769e796d6c070
jaloliddin <jaloliddintemirov@gmail.com>
2024-10-08 14:34:13 +0500

    Commited

---
+++ src/test/java/tmrv/dev/blogsystem/BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tmrv.dev.blogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

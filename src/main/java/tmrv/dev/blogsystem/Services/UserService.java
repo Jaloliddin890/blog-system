d45d8a03d1f3e869b56b5bbdb422e12cdb597dc3
Jaloliddin890 <jaloliddintemirov890@gmail.com>
2024-11-26 22:21:21 +0500

    Done

--- src/main/java/tmrv/dev/blogsystem/Services/UserService.java
+++ src/main/java/tmrv/dev/blogsystem/Services/UserService.java
@@ -40,9 +40,7 @@ public class UserService {
     @Transactional
     public String unBlockUser(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
-        if (user.getRole().equals(Role.ADMIN)) {
-            return "Cannot block admin role. Admin ID: " + user.getId();
-        }
+        if (user.getRole().equals(Role.ADMIN)) return "Cannot block admin role. Admin ID: " + user.getId();
         user.setEnabled(true);
         userRepository.save(user);
         return "Blocked user's id: " + user.getId();
@@ -50,9 +48,7 @@ public class UserService {
 
     @Transactional
     public void deleteUser(Long id) {
-        if (!userRepository.existsById(id)) {
-            throw new UserNotFoundException("User not found with id: " + id);
-        }
+        if (!userRepository.existsById(id)) throw new UserNotFoundException("User not found with id: " + id);
         userRepository.deleteById(id);
     }
 
